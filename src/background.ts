d079be9a5a96fe519880afcf12224d65134aa21e
Mario Martinez Agundez <mmartinezagundez@gmail.com>
2022-07-25 16:45:29 +0200

    .

---
+++ src/background.ts
@@ -0,0 +1,10 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.webNavigation.onCompleted.addListener(() => {
+      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        if (id) {
+          chrome.action.disable(id);
+        }
+      });
+    }, 
+    { url: [{ hostContains: 'google.com' }] });
+  });
