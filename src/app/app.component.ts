22a15493bf21dbcc8e7b6174d5604fb0fa722baf
Mario Martinez Agundez <mmartinezagundez@gmail.com>
2022-07-26 12:08:38 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chome-extension-test';
+  url = "";
+  isBusy: boolean = false;
+
+  ngOnInit(): void {
+    
+    
+    
+  }
+
+  async getURL ()
+  {
+    
+    console.log("click!");
+    
+    const tabs = await chrome.tabs.query({active: true, currentWindow: true});
+    console.log(tabs[0].url);
+
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id! },
+      func: () => {
+        console.log("updateBackgroundColor");
+        document.body.style.backgroundColor = "#FF0000";
+        
+      },
+      args: []
+    });
+
+    // if (tabs[0].url) {
+    //   this.url = tabs[0].url;
+    // }
+    
+    
+  }
+
+  
 }
+
+const updateBackgroundColor = () => { 
+  console.log("updateBackgroundColor");
+  
+  document.body.style.backgroundColor = "#FF0000";
+};
--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.webNavigation.onCompleted.addListener(() => {
-      chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
-        if (id) {
-          chrome.action.disable(id);
-        }
-      });
-    }, 
-    { url: [{ hostContains: 'google.com' }] });
+    // chrome.webNavigation.onCompleted.addListener(() => {
+    //   chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+    //     if (id) {
+    //       chrome.action.disable(id);
+    //     }
+    //   });
+    // }, 
+    // { url: [{ hostContains: 'google.com' }] });
   });
